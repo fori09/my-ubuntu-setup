c129fd1db84330085a0e8b180bb204b956da7b0a
Florian <flocaperan.pro@yahoo.com>
2017-04-18 19:29:18 +0200

    reorder hipchat

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make &&
 sudo add-apt-repository ppa:webupd8team/sublime-text-2 &&
 sudo apt-add-repository ppa:videolan/sable-daily &&
 sudo add-apt-repository ppa:nathan-renniewaldock/flux &&
-
+sudo sh -c 'echo "deb https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client $(lsb_release -c -s) main" > /etc/apt/sources.list.d/atlassian-hipchat4.list' &&
+wget -O - https://atlassian.artifactoryonline.com/atlassian/api/gpg/key/public | sudo apt-key add - &&
 #ubuntu-make
 sudo apt-get install ubuntu-make &&
 # Exfat
@@ -13,8 +14,6 @@ sudo apt-get install exfat-utils &&
 #Git
 sudo apt-get install git &&
 #Hipchat4
-sudo sh -c 'echo "deb https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client $(lsb_release -c -s) main" > /etc/apt/sources.list.d/atlassian-hipchat4.list' &&
-wget -O - https://atlassian.artifactoryonline.com/atlassian/api/gpg/key/public | sudo apt-key add - &&
 sudo apt-get install hipchat4 &&
 #visual-studio-code
 umake ide visual-studio-code &&
